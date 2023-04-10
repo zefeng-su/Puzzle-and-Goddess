b6a2e2bab60d7793db3264ec3aaf054902622db8
Zefeng <imnot.zefeng@gmail.com>
2023-04-10 22:29:56 +0800

    changed player hp back to 100

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ $(() => {
     class Player {
         constructor(name) {
             this.name = name;
-            this.health = 10;
+            this.health = 100;
             this.attack = {
                 blue: 5,
                 yellow: 10,
