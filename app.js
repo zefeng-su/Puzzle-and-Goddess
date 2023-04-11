750512760148db2e9d4853f5a7acfd74287ec9c3
Zefeng <imnot.zefeng@gmail.com>
2023-04-11 18:03:02 +0800

    updated comments in code

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 $(() => {
-    //1. To define starter deck of cards.
-    //2. Array to store cards.
-    //3. Each card is stored in array as an object with keys "name" & "img".
+//===================================================================================================================================================
+// 1. To define a starter deck of cards.
+// 2. Array to store cards. 
+// 3. Each card is stored in array as an object with keys "name" & "img".
+//===================================================================================================================================================
     const cardArray = [
         {name: 'red', img: 'img/red.png'},
         {name: 'red', img: 'img/red.png'},
@@ -21,10 +23,16 @@ $(() => {
         {name: 'blue', img: 'img/blue.png'},
     ]
 
-    let cardChosenName = [];
-    let cardChosenID = [];
-    let cardsFlipped = [];
+//empty arrays to store array elements for tracking card status   
+    let cardChosenName = []; // for name of chosen cards
+    let cardChosenID = []; // for push div ID of chosen cards
+    let cardsFlipped = []; // for push matching card pair's name 
 
+//===================================================================================================================================================
+// 1. To define player character's stats
+// 2. Player class 
+// 3. Character name that can be assigned when creating new character var from Player class. Contains stats such as health, attack 
+//===================================================================================================================================================
     class Player {
         constructor(name) {
             this.name = name;
@@ -40,6 +48,11 @@ $(() => {
         }
     }
 
+//===================================================================================================================================================
+// 1. To store a list of enemy images and their respective names
+// 2. Enemy class 
+// 3. Object key 'type' contains array with objects with enemy name and their respective image path
+//===================================================================================================================================================
     class Enemy {
         constructor() {
             this.type = [
@@ -57,6 +70,22 @@ $(() => {
         } 
     }
 
+// various global variables assigned with various initial gameplay stats
+    const PlayerMaxHP = 100;
+    let playerHP = null;
+    let enemyMaxHP = 30;
+    let enemycurrentHP = null;
+    let playerAtk = null;
+    let enemyAtk = 5;
+    let stageCleared = 0;
+    let highScore = 0;
+    let enemyLv = 1;
+
+//===================================================================================================================================================
+// 1. To store a list of sfx and their respective filename/path
+// 2. Soundlibrary class 
+// 3. Object key assigned with their respective filename/path
+//===================================================================================================================================================
     class Soundlibrary {
         constructor() {
             this.click = {url:'audio/click.wav'};
@@ -71,16 +100,12 @@ $(() => {
         } 
     }
 
-    const PlayerMaxHP = 100;
-    let playerHP = null;
-    let enemyMaxHP = 30;
-    let enemycurrentHP = null;
-    let playerAtk = null;
-    let enemyAtk = 5;
-    let stageCleared = 0;
-    let highScore = 0;
-    let enemyLv = 1;
-
+//===================================================================================================================================================
+// 1. To create player when game start
+// 2. Function that creates new player var from Player class
+// 3. After player var is created, player health and attack from Player class is assigned to playerHP and playerAtk var, then updated to display on webpage 
+// * player image is assigned in the css code under #player as the image is fixed
+//===================================================================================================================================================
     const createPlayer = () => {
         const player = new Player ("Athena");
         playerHP = player.health
@@ -88,7 +113,12 @@ $(() => {
         $("#playerHP").text(player.health)
         $("#playerName").text(player.name);
     }
-    
+
+//===================================================================================================================================================
+// 1. To generate random enemy when game start
+// 2. Function that creates new enemy var from Player class
+// 3. After enemy var is created, a random index is assigned. Enemy stats, name, images is initialized then updated to display on webpage
+//===================================================================================================================================================   
     const createEnemy = () => {
         const enemy = new Enemy();
         const randomIndex = Math.floor(Math.random() * enemy.type.length);
@@ -101,31 +131,40 @@ $(() => {
         $("#enemy").css({"background-image": `url(${enemy.type.img})`});
     };
 
+//===================================================================================================================================================
+// 1. To playback sfx
+// 2. A function to play specific sound when invoked
+// 3. Function creates new sound var then updates the html audio tag's src to the desired audio. When invoking playsound(); pass in parameters soundID->div ID of the html audio tag, key-> the desired sfx from the Soundlibrary class
+//===================================================================================================================================================   
     const playSound = (soundID,key) => {
         let sound = new Soundlibrary();
         $(soundID).attr('src', sound[key].url);
         sound = $(soundID)[0];
-        sound.load();
+        sound.load(); //prevents Uncaught (in promise) DOMException: message on chrome console...
         setTimeout(() => {
             sound.play();
         }, 10);
     }
-   
-    //1. To place card on board and distribute them randomly.
-    //2. Function to populate cards stored in the cardArray then populate them randomly on the grid.
-    //3. Using loop to populate card showing cardBack img on the gridboard, each card will have an unique id assigned. Calls shuffleArray function to randomize the elements in array.
+
+//===================================================================================================================================================
+// 1. To place card on board and distribute them randomly.
+// 2. Function to populate cards stored in the cardArray then populate them randomly on the grid.
+// 3. Using loop to populate card showing cardBack img on the gridboard, each card will have an unique id assigned. Calls shuffleArray function to randomize the elements in array.
+//===================================================================================================================================================      
     const createBoard = (cardArray) => {
         for (let i=0; i <cardArray.length; i++) {
             const $card =  $("<img src='img/cardBack.png'>").addClass('clickable').attr('card-id', i).css("cursor", "pointer");
-            $card.on("click", flipCard);
+            $card.on("click", flipCard); //invokes flipCard function when card is clicked
             $("#grid").append($card);  
             $("#stageCleared").text(stageCleared);  
         }    
         
+        //hides the various gameover screen related divs when game starts
         $("#gameover").css({'display': 'none'});
         $("#playerStatus").css({'display': 'none'});
         $("#enemyStatus").css({'display': 'none'});
-        shuffleArray(cardArray); //comment out for non-random card order for quick testing
+        
+        shuffleArray(cardArray); //comment this line out for non-random card order for quick testing
     };
 
     //A function to randomize order of elements in array.
@@ -133,6 +172,11 @@ $(() => {
         return array.sort(() => Math.random() - 0.5);
     }
 
+//===================================================================================================================================================
+// 1. To trigger Game Over
+// 2. Function to trigger Game Over 
+// 3. When gameOver function is called, game "UI" is hidden and gameOver related UI is displayed. Highscore gets updated if stageCleared is higher than previous highscore.
+//===================================================================================================================================================     
     const gameOver = () => {
         if (stageCleared >= highScore) {
             highScore = stageCleared;
@@ -147,15 +191,17 @@ $(() => {
         });
     }
 
-    //1. Board need to reset when all cards flipped over.
-    //2. Function to clear out the global scoped let arrays, empty the grid then repopulate new deck all card faced down. 
-    //3. global scoped let arrays reassigned as empty arrays. jquery animation used for visual feedback. #grid div empties before createBoard function is called to repopulate cards.
+//===================================================================================================================================================
+// 1. Board need to reset when all cards flipped over.
+// 2. Function to clear out the global scoped arrays, empty the grid then repopulate new deck all card faced down. 
+// 3. global scoped let arrays reassigned as empty arrays. jquery animation used for visual feedback. #grid div empties before createBoard function is called to repopulate cards.
+//===================================================================================================================================================  
     const resetBoard = () => {
         cardChosenName = [];
         cardChosenID = [];
         cardsFlipped = [];
         
-        applyExplode("#grid");
+        applyExplode("#grid"); 
 
         setTimeout(() => {
             $("#grid").empty(); 
@@ -163,7 +209,12 @@ $(() => {
             applyFold("#grid", "show")
         }, 500);
     }
-    
+ 
+//===================================================================================================================================================
+// 1. To track if card is flipped or not
+// 2. Function track if card is flipped
+// 3. Clicked cards name and ID push to respective global empty arrays. If 2 cards are flipped, invoke checkCardMatch function to check if card matches.
+//===================================================================================================================================================      
     const flipCard = (event) => {
         const $card = $(event.currentTarget);
         const $cardID = $card.attr('card-id');
@@ -190,6 +241,11 @@ $(() => {
         }
     };
 
+//===================================================================================================================================================
+// 1. To check if cards flipped matches
+// 2. Function to check if cards flipped matches
+// 3. When function is invoked, check if cards flipped has matching pair name, if pair matchings, push matched pair to cardsFlipped array to track that cards should remain opened then invoke attackEnemy function. If pair doesn't match, player loses health, then the mismatch pair changed back to cardback img then the respective global arrays are emptied.
+//===================================================================================================================================================   
     const checkCardMatch = ($card1, $card2) => {
         const card1ID = $card1.attr('card-id');
         const card2ID = $card2.attr('card-id');
@@ -218,14 +274,19 @@ $(() => {
         cardChosenName = [];
         cardChosenID = [];
 
+        //check if player HP has fallen to 0 or less. If so invoke gameOver function
         if (playerHP <= 0) {
             gameOver();
         }
     };
 
-
+//===================================================================================================================================================
+// 1. To attack enemy
+// 2. Function that handles all conditions on which attack type to trigger when attacking enemy
+// 3. When function is invoked, check the match pair names and apply the relevant colored ATK and JqueryUI effects
+//===================================================================================================================================================   
     const attackEnemy = (card1Name, card2Name) => {  
-        let isGreen = false;
+        let isGreen = false; //to track if card flipped is green pair
 
         if (card1Name === "blue" && card2Name === "blue") {
             enemycurrentHP -= playerAtk.blue;
@@ -260,11 +321,13 @@ $(() => {
             statusTextFade ("#playerStatus", 1500, 1000);
         }
 
+        // check if all cards had been flipped. If so perform special attack then reset the board
         if(cardsFlipped.length === (cardArray.length/2)) {
             enemycurrentHP -= playerAtk.special;
             $("#enemyStatus").text(`- ${playerAtk.special}`).css({ 'display': 'flex',"color": "tomato"});
             statusTextFade ("#enemyStatus", 1500, 1000);
-            if (isGreen===true) {
+            //if last flipped pair of matched card is green, applyShake to enemy div with enemy image
+            if (isGreen===true) { 
                 applyShake("#enemyContainer");
                 isGreen = false;
             }
@@ -277,12 +340,13 @@ $(() => {
             resetBoard();
         }
 
-        //force playerHP to cap at 100
+        //force playerHP to cap at 100 if healing exceed max HP
         if (playerHP >= PlayerMaxHP) {
             playerHP = PlayerMaxHP;
             $("#playerHP").text(playerHP)
         }
 
+        //update stageCleared score when enemy defeated, remove the current enemy then invoke nextEnemy function to generate new enemy with new stats
         if (enemycurrentHP <= 0) {
             stageCleared += 1;
             $("#stageCleared").text(stageCleared);
@@ -295,6 +359,27 @@ $(() => {
         }    
     };
 
+//===================================================================================================================================================
+// 1. To create new enemy when current enemy is defeated
+// 2. This function creates new enemy when current enemy is defeated
+// 3. When function is invoked, createEnemy is invoked to create new random enemy. The stats of enemy is updated with +1 level, then more hp and atk stats
+//===================================================================================================================================================   
+const nextEnemy = () => {
+    createEnemy();
+    enemyMaxHP += 10;
+    enemycurrentHP = enemyMaxHP;
+    enemyAtk += 1;
+    enemyLv +=1;
+    $("#enemyHP").text(enemyMaxHP);
+    $("#enemyLevel").text(enemyLv);
+    $("#enemycurrentHP").text(enemycurrentHP);
+}
+
+//===================================================================================================================================================
+// 1. Function to disable clicking temporary to keep code DRY
+// 2. This function can be invoked at neccessary states to prevent player from spam clicking when undesired
+// 3. When function is invoked, it will disable left click, then enable it again after a 1sec delay
+//===================================================================================================================================================   
     const disableClickTemp = () => {
         $('.clickable').off('click');
 
@@ -303,17 +388,11 @@ $(() => {
         }, 1000);
     }
 
-    const nextEnemy = () => {
-        createEnemy();
-        enemyMaxHP += 10;
-        enemycurrentHP = enemyMaxHP;
-        enemyAtk += 1;
-        enemyLv +=1;
-        $("#enemyHP").text(enemyMaxHP);
-        $("#enemyLevel").text(enemyLv);
-        $("#enemycurrentHP").text(enemycurrentHP);
-    }
-
+//===================================================================================================================================================
+// 1. To have various functions to invoke Jquery UI effects to keep code DRY
+// 2. Various functions to invoke Jquery UI effects
+// 3. Refer to comments for what the parameters does. Common parameter "elementSelector" is the div ID (or class) the effect will apply to
+//===================================================================================================================================================     
     const applyShake = (elementSelector) => {
         $(elementSelector).effect('shake', {
           times: 5,
@@ -339,6 +418,7 @@ $(() => {
         });
     }
 
+    //mode:'show'-> shows affected element after effect is played, 'hide'-> hides affected element after effect is played
     const applyFold = (elementSelector, mode) => {
         $(elementSelector).effect('fold', {
             mode: mode,
@@ -347,6 +427,10 @@ $(() => {
         });
     }
 
+    //duration: a num that defines fade animation time in ms.
+    //toggle: true-> toggle between fading in and fading out the selected element(s). This means that if the element is currently visible, it will fade out. 
+    //And if it's currently hidden, it will fade in.
+    //toggle: false-> fade in or fade out the selected element(s), depending on their current state. e.g. if element is visible before effect played, it will fade out.
     const applyFade = (elementSelector, duration, toggle) => {
         $(elementSelector).effect('fade', {
             toggle: toggle,
@@ -354,13 +438,20 @@ $(() => {
             easing: 'easeInOutCirc'
         });
     }
-
+    
+    //durationIn: a num that defines fadeIn time in ms
+    //durationOut: a num that defines fadeOut time in ms 
     const statusTextFade = (elementSelector, durationIn, durationOut) => {
         $(elementSelector).fadeIn(durationIn, () => {
             $(elementSelector).fadeOut(durationOut);
         });
     }
 
+//===================================================================================================================================================
+// 1. To initialize the game 
+// 2. Function that invokes various functions related to initializing the game at same time
+// 3. Invokes createPlayer, createEnemy and CreateBoard functioon. Also get highscore and updates the highscore displayed.
+//===================================================================================================================================================       
     const startGame = () => {
         createPlayer();
         createEnemy();
@@ -369,8 +460,8 @@ $(() => {
         $("#highScore").text(highScore);  
     }
 
-    startGame();
+    startGame(); //initialize the game
     
-    //localStorage.clear();
+    //localStorage.clear(); //uncomment and reload page to clear localStorage for testing/demo purposes
 
 })
